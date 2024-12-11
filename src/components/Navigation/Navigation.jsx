f7963274f0f9b9116698e8c9ad1496bf4e79b697
Alex <zemzikova.oleksandra@gmail.com>
2024-12-11 21:18:45 +0200

    n und

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import styles from "./Navigation.module.css";
 
 const Navigation = () => {
   return (
--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../../redux/auth/operations";
 import { selectUser } from "../../redux/auth/selectors";
-import styles from "./UserMenu.module.css";
 
 const UserMenu = () => {
   const dispatch = useDispatch();
